f528a4a41c86ea44820ccd80a9cbaea0351e1b42
user.email <nahuelgallegos99@hotmail.com>
2023-02-27 20:13:58 -0300

    Manejo de archivos

--- products.js
+++ products.js
@@ -1,68 +1,85 @@
-class Products {
-    constructor (){
+import fs from 'fs/promises'
+
+class ProductManager {
+    constructor(path) {
+        this.id = 0;
+        this.path = path
         this.products = [];
     }
-    addProduct(item) {
-        const products = this.getProducts();
-        const agregadoConExito = 'Producto agregado con exito'
-        if(products.length){
-            products.find(element => {
-                if(element.code === item.code){
-                    return console.log('El producto ya a sido agregado');
-                } else {
-                    let lastIndex = products.length - 1
-                    let lastId = products[lastIndex].id;
-                    item.id = lastId + 1;
-                    let id = item.id;
-                    this.products.push(item);
-                    console.log(agregadoConExito)
-                    return id;
-                    }
-            })
-        } else {
-            item.id = 1;
-            this.products.push(item);
-            console.log(agregadoConExito)
-        }
-        }
-    getProducts() {
-        const products = this.products;
-        return products;
 
+    async cargarProducts(){
+        const json = await fs.readFile(this.path, 'utf-8');
+        if (!json) {
+            await this.guardarProducts();
+        } else {
+            const products = JSON.parse(json);
+            if (products.length > 0){
+                this.products = products;
+                this.id = this.products[this.products.length - 1].id; 
+            }
+        }    
     }
-    getProductById(id){
-        const products = this.getProducts();
-        let productsById;
-        const notFound = 'No Funciona'
-        products.map(el => {
-            el.id === id && (productsById = el);
-        });
-        return productsById ? console.log(productsById) : console.log(notFound);
+
+    async guardarProducts(){
+        const json = JSON.stringify(this.products, null, 2)
+        await fs.writeFile(this.path, json)
+    }
+
+    async agregarProduct(title, description, price, thumbail, code, stock){
+
+        await this.cargarProducts();
+        if (title && description && price && thumbail && code && stock){
+            const existCode = this.products.some(product => product.code === code) 
+            if (existCode){
+                throw new Error("ya existe");
+            } else {
+                this.products.push({ id: ++this.id, title, description, price, thumbail, code, stock});
+                await this.saveProducts(); 
+            }
+        } else {
+            throw new Error("Faltan campos");
+        }  
+    }
+
+    async getProducts(){
+        await this.cargarProducts();
+        console.log(this.products)
+    }
+
+    async getProductById(id){
+        await this.cargarProducts();
+        const indexID = this.products.find(product => product.id == id);
+        if (indexID){
+            console.log("El producto con el id es: ", indexID);
+        } else {
+            throw new Error("Not Found")
+        }
+    }
+
+    async updateProduct(id, data){
+        await this.cargarProducts();
+        const indexID = this.products.findIndex(product => product.id === id);
+        if (indexID !== -1){
+            this.products[indexID] = {
+                ...this.products[indexID], 
+                ...data
+            }
+            await this.guardarProducts();
+        } else {
+            throw new Error("Not Found")
+        }
+    }
+
+    async borrarProduct(id){
+        await this.cargarProducts();
+        const existID = this.products.findIndex(product => product.id === id);
+        if (existID !== -1){
+            this.products.splice(existID, 1)
+            await this.saveProducts()
+        } else {
+            throw new Error("No Funciona")
+        }
     }
 }
 
-const almacenarProducts = new Products();
-
-console.log(almacenarProducts.getProducts())
-
-almacenarProducts.addProduct({
-    title:'Hoddie black',
-    description:'Corte y diseño OVERSIZED EXTRA , esta hoodie provee el ajuste extra holgado en las mangas, hombros y pecho del usuario, aportando un aspecto relajado y sutil urbano, ideal para combinar con shorts, mallas o pantalones.',
-    price:200,
-    thumbnail:'',
-    code:'02',
-    stock:10
-});
-
-console.log(almacenarProducts.getProducts());
-
-almacenarProducts.addProduct({
-    title:'Remera Blanca',
-    description:'Corte y diseño OVERSIZED, esta camiseta provee el ajuste extra holgado en las mangas, hombros y pecho del usuario, aportando un aspecto relajado y sutil urbano.',
-    price:200,
-    thumbnail:'',
-    code:'02',
-    stock: 10
-});
-
-almacenarProducts.getProductById(2);
+const products = new ProductManager();
